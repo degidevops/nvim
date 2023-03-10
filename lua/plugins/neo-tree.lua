6e4f29e1cd3b14639b0b4a2659f901670a323e55
degidevops <degidevops@gmail.com>
2023-03-10 16:56:45 +0700

    Changes to be committed: 	new file:   .neoconf.json 	new file:   init.lua 	new file:   lazy-lock.json 	new file:   lua/config/autocmds.lua 	new file:   lua/config/keymaps.lua 	new file:   lua/config/lazy.lua 	new file:   lua/config/options.lua 	new file:   lua/plugins/example.lua 	new file:   lua/plugins/neo-tree.lua 	new file:   stylua.toml

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = true,
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    }
+  }
+}
